861440713cbbae822b7d8e9546f6d324a89655c9
Maxim Michkov <m.michkov@arenadata.io>
2024-09-24 12:48:48 +0300

    Fix GUC sync leaving broken temp tables (#1193)
    Before this patch, if an error happened during GUC sync (for example a segment
    was disconnected) the gang was destroyed but the session wasn't reset, so
    temporary tables weren't cleaned up. This left a temporary table left on
    coordinator, but not on segments, resulting in segment errors. This patch
    fixes this by enabling session reset for errors during GUC syncs.
    
    Changes from original commit:
    1. Replace CheckForResetSession with equivalent GpResetSessionIfNeeded and
    GpDropTempTables.
    2. Fix test output.
    
    Ticket: ADBDEV-6277
    (cherry picked from commit 6fe7e5316590c563c3d134777f5e93899372a317)

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -1651,7 +1651,9 @@ send_guc_to_QE(List *guc_list, bool is_restore)
 			/* if some guc can not restore successful
 			 * we can not keep alive gang anymore.
 			 */
-			DisconnectAndDestroyAllGangs(false);
+			DisconnectAndDestroyAllGangs(true);
+			GpResetSessionIfNeeded();
+			GpDropTempTables();
 			/*
 			 * when qe elog an error, qd will use ReThrowError to
 			 * re throw the error, the errordata_stack_depth will ++,
